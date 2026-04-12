afdcf35632a8f8a08f2db5fdb4cb338b58bc7d03
thebhagavatula <bhagavatulasrikarthikeya@gmail.com>
2026-04-12 16:58:47 +0000

    add: escrow, reputation score, stream_payment, swarm_bus skeleton to dir chain

---
+++ chain/programs/stream_payment/src/lib.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+declare_id!("2hYgwHGvPEpqSumayofrqiqLfpyqJadLRMad1tfLtuoW");
+
+#[program]
+pub mod escrow {
+    use super::*;
+}
---
+++ chain/programs/swarm_bus/src/lib.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+declare_id!("G9ADiy7bb4bfqjEbihS8Mfaq1VSv7NvVRzyiQrFVjDSE");
+
+#[program]
+pub mod escrow {
+    use super::*;
+}
